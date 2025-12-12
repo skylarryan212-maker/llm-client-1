3c4949840fb4eb687abff996576dc5f0bd00df16
Skylar Ryan <sryan3000@outlook.com>
2025-12-12 16:14:54 -0500

    Keep human-writing chat content scrollable

--- new-client/app/agents/human-writing/c/[chatId]/page.tsx
+++ new-client/app/agents/human-writing/c/[chatId]/page.tsx
@@ -239,7 +239,7 @@ function ChatInner({ params }: PageProps) {
         </div>
       </header>
 
-      <main className="flex-1 overflow-hidden">
+      <main className="flex-1 min-h-0 overflow-hidden">
         <ScrollArea className="h-full">
           <div className="py-4">
             <div className="w-full px-4 sm:px-6 lg:px-12">
